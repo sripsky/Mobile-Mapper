09ce6e4fb83c1da6171e09c9d4861297ca0f56ea
Sydney Ripsky <sripsky@bsd220.org>
2020-02-24 10:31:59 -0600

    Initial Commit

---
+++ Mobile Mapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mobile Mapper
+//
+//  Created by Sydney Ripsky on 2/24/20.
+//  Copyright © 2020 Sydney Ripsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
